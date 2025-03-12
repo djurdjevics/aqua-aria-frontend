952fa40e42223efc1770f6ff6773d9e729547334
Jovan Cvetkov <yo1.cvetkov@gmail.com>
2025-03-12 17:11:49 +0100

    Our mission section refactored

--- src/components/pages/root/our-mission/OurMissionImage.tsx
+++
@@ -1,7 +0,0 @@
-export function OurMissionImage() {
-  return (
-    <div className="hidden h-full w-full overflow-hidden rounded-2xl md:block">
-      <img src="/our-mission-img.jpg" className="h-full w-full" />
-    </div>
-  );
-}
