8da65090c03e5cb4bb779e0630963c983e02bdc9
Jovan Cvetkov <yo1.cvetkov@gmail.com>
2025-03-16 17:04:47 +0100

    Moved wave a bit down

--- src/assets/waves/WaveSecondary.tsx
+++ src/assets/waves/WaveSecondary.tsx
@@ -21,7 +21,7 @@ export function WaveSecondary({ className }: { className?: string }) {
 export function WaveSecondaryMobile() {
   return (
     <svg
-      className="absolute inset-x-0 bottom-0 w-full"
+      className="absolute inset-x-0 -bottom-[20px] w-full"
       // width="375"
       // height="39"
       viewBox="0 0 375 39"
